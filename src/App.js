fcfcd4bced4e6c55c9a8e9fcd53cf6f4b809d9dd
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-12 13:23:10 +0530

    🚀Update: styling components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import './styles/index.css';
+import Header from './components/section-components/Header';
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <Header/>
     </div>
   );
 }
